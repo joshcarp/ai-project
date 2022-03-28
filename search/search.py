48a792ca9c436d787b462881e10922b4587ca311
joshcarp <joshuacarpeggiani@gmail.com>
2022-03-28 22:27:04 +1100

    Attempt A* number 1, apparently i don't know how to implement the algorithm lol

--- search/search.py
+++ search/search.py
@@ -3,12 +3,19 @@ from typing import List
 
 
 class Hexagon:
+    pass
+
+
+class Hexagon:
     coords: (int, int)
     color: str
+    previous: Hexagon
+    path_cost: int
 
     def __init__(self, i: int, j: int):
         self.coords = (i, j)
         self.color = ""
+        self.path_cost = 10000000000
 
     def __repr__(self):
         return f"{self.coords} {self.color}"
@@ -16,11 +23,6 @@ class Hexagon:
     def __add__(self, other):
         return Hexagon(self.coords[0] + other.coords[0],
                        self.coords[1] + other.coords[1])
-
-
-def neighbours(self: Hexagon, n: int) -> {(int, int)}:
-    return {(self + a).coords for a in direction_vectors() if
-            valid((self + a).coords, n)}
 
 
 def valid(a: (int, int), n: int) -> bool:
@@ -51,9 +53,10 @@ class Board:
     n: int
 
     def __init__(self, input: Input):
+        if input is None:
+            return
         self.n = input.n
         self.pieces = []
-
         for i in range(input.n):
             self.pieces.append([])
             for j in range(input.n):
@@ -65,11 +68,25 @@ class Board:
         for elem in input.board:
             self.piece(elem[1], elem[2]).color = elem[0]
 
+    def copy(self):
+        a: Board = Board(None)
+        a.__dict__ = self.__dict__
+        return a
+
     def __repr__(self):
         return f"{self.pieces}"
 
     def piece(self, x: int, y: int) -> Hexagon:
         return self.pieces[x][y]
 
+    def piece_tuple(self, x: (int, int)) -> Hexagon:
+        return self.pieces[x[0]][x[1]]
+
     def color(self, loc: (str, int, int)):
         self.pieces[loc[1]][loc[2]].color = loc[0]
+
+
+def neighbours(self: Hexagon, board: Board) -> [Hexagon]:
+    return [board.piece_tuple((self + a).coords) for a in direction_vectors()
+            if
+            valid((self + a).coords, board.n)]
--- search/search_test.py
+++ search/search_test.py
@@ -1,3 +1,5 @@
+import typing
+
 import search
 
 
@@ -20,30 +22,54 @@ def testsquare():
     print(b)
     print(b.pieces[3][2])
     print(search.direction_vectors()[0] + b.pieces[3][2])
+    end = find_end(b)
+    print(end)
 
 
-def testneighbours():
-    n = 5
-    tests = [
-        {
-            "coord": (0, 0),
-            "neighbours": {(1, 0), (0, 1)}
-        },
-        {
-            "coord": (0, 1),
-            "neighbours": {(0, 0), (1, 0), (1, 1), (0, 2)}
-        },
-        {
-            "coord": (2, 2),
-            "neighbours": {(2, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 1)}
-        },
-        {
-            "coord": (4, 4),
-            "neighbours": {(4, 3), (3, 4)}
-        },
-    ]
+def find_end(b: search.Board):
+    current = b.start
+    closed: typing.List[search.Hexagon] = []
+    open: typing.List[search.Hexagon] = []
+    current.path_cost = 0
+    while current.coords != b.goal.coords:
+        closed.append(current)
+        for elem in search.neighbours(current, b):
+            current_path_cost = current.path_cost + 1
+            if elem.path_cost < current.path_cost:
+                if elem in closed:
+                    continue
+                elif elem in open:
+                    elem.path_cost = current_path_cost
+                    current = elem
+                    elem.previous = current
+            if elem not in closed and elem not in open:
+                elem.path_cost = current_path_cost
+                open.append(elem)
+    return current
 
-    for elem in tests:
-        coord = elem["coord"]
-        neighbours = search.neighbours(search.Hexagon(coord[0], coord[1]), n)
-        assert elem["neighbours"] == neighbours
+# def testneighbours():
+#     board: search.Board = search.Board(None)
+#     board.n = 5
+#     tests = [
+#         {
+#             "coord": (0, 0),
+#             "neighbours": {(1, 0), (0, 1)}
+#         },
+#         {
+#             "coord": (0, 1),
+#             "neighbours": {(0, 0), (1, 0), (1, 1), (0, 2)}
+#         },
+#         {
+#             "coord": (2, 2),
+#             "neighbours": {(2, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 1)}
+#         },
+#         {
+#             "coord": (4, 4),
+#             "neighbours": {(4, 3), (3, 4)}
+#         },
+#     ]
+#
+#     for elem in tests:
+#         coord = elem["coord"]
+#         neighbours = search.neighbours(search.Hexagon(coord[0], coord[1]), board)
+#         assert elem["neighbours"] == neighbours
